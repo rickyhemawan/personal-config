82b84ecdde3f4435b0ddb348a0d89353a3523417
rickyhemawan <rickyhemawan23@gmail.com>
2021-03-27 20:41:22 +0800

    global npm added

---
+++ npm-global.sh
@@ -0,0 +1,10 @@
+npm i @vue/cli -g
+npm i @vue/cli-service-global
+npm i firebase-tools -g
+npm i json-server -g
+npm i live-server -g
+npm i nodemon -g
+npm i npm -g
+npm i prettier -g
+npm i sequelize-cli -g
+npm i parcel-bundler@1.12.3 -g
